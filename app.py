3b4c3af4666393ec5da829bf6758b7140cc4c6de
nitishcodes <nitish532002@gmail.com>
2023-10-18 23:25:46 +0530

    third commit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def cosine_similarity(document1, document2):
         np.linalg.norm(vectors[0]) * np.linalg.norm(vectors[1])
     )
     content = "The value you obtained from cosine similarity indicates the degree of similarity between the two documents. It ranges from -1 (completely dissimilar) to 1 (perfectly similar), with 0 representing no similarity. The closer the value is to 1, the more similar the documents are in terms of their content, while values closer to -1 indicate dissimilarity."
-    # print("Similarity Percentage:", similarity_percentage)
+
     return [cosine_similarity, content]
 
 
@@ -81,39 +81,31 @@ def smith_waterman(document1, document2, match=2, mismatch=-1, gap=-1):
 
 
 def needleman_wunsch(doc1, doc2, match_score=2, mismatch_score=-1, gap_penalty=-2):
-    # Initialize a matrix to store the scores
     rows, cols = len(doc1) + 1, len(doc2) + 1
     score_matrix = np.zeros((rows, cols))
 
-    # Initialize the first row and column with gap penalties
     for i in range(rows):
         score_matrix[i][0] = i * gap_penalty
     for j in range(cols):
         score_matrix[0][j] = j * gap_penalty
 
-    # Fill in the matrix based on match/mismatch and gap penalties
     for i in range(1, rows):
         for j in range(1, cols):
             if doc1[i - 1] == doc2[j - 1]:
-                # Match
                 match = score_matrix[i - 1][j - 1] + match_score
             else:
-                # Mismatch
                 match = score_matrix[i - 1][j - 1] + mismatch_score
-            # Gap penalties
+
             delete = score_matrix[i - 1][j] + gap_penalty
             insert = score_matrix[i][j - 1] + gap_penalty
 
-            # Choose the maximum score among match, delete, and insert
             score_matrix[i][j] = max(match, delete, insert)
 
-    # The last cell in the matrix contains the Needleman-Wunsch score
     content = "The value obtained from the Needleman-Wunsch algorithm represents a similarity score that quantifies the degree of global sequence alignment between two documents or sequences. This score measures the overall similarity between the entire content of the documents. A higher Needleman-Wunsch score suggests a stronger global similarity, with the specific value indicating the strength of the match. "
     return [score_matrix[rows - 1][cols - 1], content]
 
 
 def minhash_lsh_similarity(doc1, doc2, num_perm=128, threshold=0.5):
-    # Create MinHash objects for both documents
     def create_minhash(document, num_perm):
         minhash = MinHash(num_perm=num_perm)
         for word in document.split():
@@ -123,13 +115,10 @@ def minhash_lsh_similarity(doc1, doc2, num_perm=128, threshold=0.5):
     minhash1 = create_minhash(doc1, num_perm)
     minhash2 = create_minhash(doc2, num_perm)
 
-    # Create an LSH index
     lsh = MinHashLSH(threshold=threshold, num_perm=num_perm)
 
-    # Index the first document
     lsh.insert("doc1", minhash1)
 
-    # Query the LSH index with the second document
     result = lsh.query(minhash2)
     content = "The value obtained using MinHash and LSH represents an estimate of the Jaccard similarity between the two documents. It's a measure of how many unique elements (e.g., words or tokens) the two documents share, relative to their total unique elements. The higher the value, the more similar the documents are in terms of their content, and vice versa."
 
@@ -168,23 +157,21 @@ def main():
         document1 = request.files["doc1"]
         document2 = request.files["doc2"]
         choice = request.form["similarity-measure"]
-        # print(document1, document2, choice)
+
         if document1.filename == "" or document2.filename == "":
             return render_template("index.html", message=0)
         else:
             filename_1 = secure_filename(document1.filename)
             document1.save(os.path.join(app.config["UPLOAD_FOLDER"], filename_1))
-            # print(os.path.join(app.config["UPLOAD_FOLDER"], filename_1))
+
             filename_2 = secure_filename(document2.filename)
-            # print(os.path.join(app.config["UPLOAD_FOLDER"], filename_2))
+
             document2.save(os.path.join(app.config["UPLOAD_FOLDER"], filename_2))
             similarity_score = calculate_similarity(
                 os.path.join(app.config["UPLOAD_FOLDER"], filename_1),
                 os.path.join(app.config["UPLOAD_FOLDER"], filename_2),
                 choice,
             )
-            # os.remove(os.path.join(app.config["UPLOAD_FOLDER"], filename_1))
-            # os.remove(os.path.join(app.config["UPLOAD_FOLDER"], filename_2))
 
             return render_template(
                 "index.html", message=similarity_score[0], content=similarity_score[1]
